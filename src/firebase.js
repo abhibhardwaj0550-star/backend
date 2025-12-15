7f5b861ab0a814ffe7799f3abc0b04792ddde338
abhibhardwaj0550-star <abhibhardwaj0550@gmail.com>
2025-12-15 11:20:04 +0530

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,14 @@
 import admin from "firebase-admin";
+import fs from "fs";
 import path from "path";
 
 const serviceAccountPath = path.resolve("./src/serviceAccount.json");
 
+// Read JSON file
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf-8"));
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountPath),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 console.log("Firebase initialized ✅");
