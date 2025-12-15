4d6adb4fcf3ec18cdf8df9b23f39fe9a037c6223
abhibhardwaj0550-star <abhibhardwaj0550@gmail.com>
2025-12-15 11:25:18 +0530

    updated code

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,10 @@ import path from "path";
 
 const serviceAccountPath = path.resolve("./src/serviceAccount.json");
 
-// Read JSON file
+if (!fs.existsSync(serviceAccountPath)) {
+  fs.writeFileSync(serviceAccountPath, process.env.FIREBASE_ADMIN_KEY);
+}
+
 const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf-8"));
 
 admin.initializeApp({
